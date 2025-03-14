cf6243c6e9ebf0526af0e5852a65fb2a7c928af1
VD <VD@VD>
2025-03-15 00:03:48 +0200

    local commit

--- src/screens/RacesScreen.tsx
+++ src/screens/RacesScreen.tsx
@@ -2,10 +2,12 @@ import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 function RacesScreen({ route }: any): React.ReactElement {
+    {console.log(JSON.stringify(route.params))}
     return (
         <SafeAreaView>
             <Text>Params:</Text>
             <Text>{JSON.stringify(route.params)}</Text>
+            
         </SafeAreaView>
     );
 }
